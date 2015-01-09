277e0b4cc424a589faa311fa9ae39c4b67135634
vilbeyli <volkanilbeyli@gmail.com>
2015-01-10 00:17:23 +0200

    Other ghosts added to scene. Wait and Scatter phases implemented.
    Ghost AI's (other than blinky's left).

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	private bool paused = false;
+	private bool quit = false;
+
+	public Canvas PauseCanvas;
+	public Canvas QuitCanvas;
+	public Button MenuButton;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			if(quit == true)
+				ToggleQuit();
+			else
+				TogglePause();
+		}
+	}
+
+	public void TogglePause()
+	{
+		// if paused before key stroke, unpause the game
+		if(paused)
+		{
+			Time.timeScale = 1;
+			PauseCanvas.enabled = false;
+			paused = false;
+			MenuButton.enabled = true;
+		}
+
+		// if not paused before key stroke, pause the game
+		else
+		{
+			PauseCanvas.enabled = true;
+			Time.timeScale = 0.0f;
+			paused = true;
+			MenuButton.enabled = false;
+		}
+	}
+
+	public void ToggleQuit()
+	{
+		if(quit)
+		{
+			PauseCanvas.enabled = true;
+			QuitCanvas.enabled = false;
+			quit = false;
+		}
+
+		else
+		{
+			PauseCanvas.enabled = false;
+			QuitCanvas.enabled = true;
+			quit = true;
+		}
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
--- Assets/Scripts/GhostMove.cs
+++ Assets/Scripts/GhostMove.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System;
 
 public class GhostMove : MonoBehaviour {
 
-	public Transform waypoint;			// AI-determined waypoint
+	private Vector3 waypoint;			// AI-determined waypoint
 	private Queue<Vector3> waypoints;	// waypoints used on Init and Scatter states
 	public bool AI = false;
 
@@ -20,25 +22,30 @@ public class GhostMove : MonoBehaviour {
 		set
 		{
 			_direction = value;
-			waypoint.position = transform.position + _direction;
+			waypoint = transform.position + _direction;
 			//Debug.Log ("waypoint (" + waypoint.position.x + ", " + waypoint.position.y + ") set! _direction: " + _direction.x + ", " + _direction.y);
 		}
 	}
 
 	public AI AIComponent;
 	public float speed = 0.3f;
-	public float scatterLength = 5f;
-	private float timeToScatter;
 
-	enum State { Init, Scatter, Chase, Run };
+	public float scatterLength = 5f;
+	public float waitLength = 0.0f;
+
+	private float timeToEndScatter;
+	private float timeToEndWait;
+
+	enum State { Wait, Init, Scatter, Chase, Run };
 	State state;
 
 	//-----------------------------------------------------------------------------------------
-	// Start of Functions
+	// variables end, functions begin
 	void Start()
 	{
-		state = State.Init;
-		//_direction = waypoint.position - transform.position;
+		waypoint = transform.position;	// to avoid flickering animation
+		state = State.Wait;
+		timeToEndWait = Time.time + waitLength;
 		InitializeWaypoints(state);
 
 	}
@@ -49,6 +56,10 @@ public class GhostMove : MonoBehaviour {
 
 		switch(state)
 		{
+		case State.Wait:
+			Wait();
+			break;
+
 		case State.Init:
 			Init();
 			break;
@@ -69,46 +80,92 @@ public class GhostMove : MonoBehaviour {
 	}
 
 	//-----------------------------------------------------------------------------------
-	// Start functions
+	// Start() functions
 	void InitializeWaypoints(State st)
 	{
+		//-----------------
+		// File Format: Init and Scatter coordinates separated by empty line
+		// Init X,Y 
+		// Init X,Y
+		// 
+		// Scatter X,Y
+		// Scatter X,Y
+
+		string path = Application.dataPath + "/Data/" + transform.name + "_wps.txt";
+		StreamReader stream = new StreamReader(path);
+
 		waypoints = new Queue<Vector3>();
 		Vector3 wp;
 
 		if(st == State.Init)
-		{
-			wp = new Vector3(15, 20, 0);
-			waypoints.Enqueue(wp);
-			wp = new Vector3(16, 20, 0);
-			waypoints.Enqueue(wp);
-			wp = new Vector3(16, 23, 0);
-			waypoints.Enqueue(wp);
-			wp = new Vector3(19, 23, 0);
-			waypoints.Enqueue(wp);
-			wp = new Vector3(19, 26, 0);
-			waypoints.Enqueue(wp);
-			wp = new Vector3(22, 26, 0);
-			waypoints.Enqueue(wp);
+		{ 
+			while(!stream.EndOfStream)
+			{
+				// stop reading if empty line is reached
+				string line = stream.ReadLine();
+				if(line.Length == 0) 	break;
+
+				string[] values = line.Split(' ');
+				float x = float.Parse(values[0]);
+				float y = float.Parse(values[1]);
+
+				wp = new Vector3(x,y,0);
+				waypoints.Enqueue(wp);
+			}
 		}
 
 		if(st == State.Scatter)
 		{
-			wp = new Vector3(27, 26, 0);
-			waypoints.Enqueue(wp);
-			wp = new Vector3(27, 30, 0);
-			waypoints.Enqueue(wp);
-			wp = new Vector3(22, 30, 0);
-			waypoints.Enqueue(wp);
-			wp = new Vector3(22, 26, 0);
-			waypoints.Enqueue(wp);
+			// skip until empty line is reached, read coordinates afterwards
+			bool scatterWps = false;	// Scatter waypoints
+			while(!stream.EndOfStream)
+			{
+				string line = stream.ReadLine();
+				if(line.Length == 0)
+				{
+					scatterWps = true;	// we reached the scatter waypoints
+					continue;	// do not read empty line, go to next line
+				}
+
+				if(scatterWps)
+				{
+					string[] values = line.Split(' ');
+					int x = Int32.Parse(values[0]);
+					int y = Int32.Parse(values[1]);
+					
+					wp = new Vector3(x,y,0);
+					waypoints.Enqueue(wp);
+				}
+			}
 		}
+
+		// if in wait state, patrol vertically
+		if(st == State.Wait)
+		{
+			Vector3 pos = transform.position;
+
+			// inky and clyde start going down and then up
+			if(transform.name == "inky" || transform.name == "clyde")
+			{
+				waypoints.Enqueue(new Vector3(pos.x, pos.y-0.5f, 0f));
+				waypoints.Enqueue(new Vector3(pos.x, pos.y+0.5f, 0f));
+			}
+			// while pinky start going up and then down
+			else
+			{
+				waypoints.Enqueue(new Vector3(pos.x, pos.y+0.5f, 0f));
+				waypoints.Enqueue(new Vector3(pos.x, pos.y-0.5f, 0f));
+			}
+		}
+
+		stream.Close();
 	}
 
 	//------------------------------------------------------------------------------------
 	// Update functions
 	void animate()
 	{
-		Vector3 dir = waypoint.position - transform.position;
+		Vector3 dir = waypoint - transform.position;
 		GetComponent<Animator>().SetFloat("DirX", dir.x);
 		GetComponent<Animator>().SetFloat("DirY", dir.y);
 	}
@@ -124,6 +181,19 @@ public class GhostMove : MonoBehaviour {
 
 	//-----------------------------------------------------------------------------------
 	// State functions
+	void Wait()
+	{
+		if(Time.time >= timeToEndWait)
+		{
+			state = State.Init;
+			waypoints.Clear();
+			InitializeWaypoints(state);
+		}
+
+		// get the next waypoint and move towards it
+		MoveToWaypoint(true);
+	}
+
 	void Init()
 	{
 		// if the Queue is cleared, do some clean up and change the state
@@ -139,27 +209,18 @@ public class GhostMove : MonoBehaviour {
 			if(name[name.Length-1] == '6' || name[name.Length-1] == '7')	direction = Vector3.down;
 
 			InitializeWaypoints(state);
-			timeToScatter = Time.time + scatterLength;
+			timeToEndScatter = Time.time + scatterLength;
 
 			return;
 		}
 
 		// get the next waypoint and move towards it
-		waypoint.position = waypoints.Peek();
-		if(transform.position != waypoint.position)
-		{
-			Vector2 p = Vector2.MoveTowards(transform.position, waypoint.position, speed);
-			rigidbody2D.MovePosition(p);
-		}
-		else
-		{
-			waypoints.Dequeue();
-		}
+		MoveToWaypoint();
 	}
 
 	void Scatter()
 	{
-		if(Time.time >= timeToScatter)
+		if(Time.time >= timeToEndScatter)
 		{
 			waypoints.Clear();
 			state = State.Chase;
@@ -167,36 +228,45 @@ public class GhostMove : MonoBehaviour {
 		}
 
 		// get the next waypoint and move towards it
-		waypoint.position = waypoints.Peek();
-		if(transform.position != waypoint.position)
-		{
-			Vector2 p = Vector2.MoveTowards(transform.position, waypoint.position, speed);
-			rigidbody2D.MovePosition(p);
-		}
-		else
-		{
-			waypoints.Enqueue(waypoints.Dequeue());
-		}
+		MoveToWaypoint(true);
+
 	}
 
 	void RunAI()
 	{
 
-		// if not at waypoint
-		if(transform.position != waypoint.position)
+		// if not at waypoint, move towards it
+		if(transform.position != waypoint)
 		{
 			AI = false;
-			Vector2 p = Vector2.MoveTowards(transform.position, waypoint.position, speed);
+			Vector2 p = Vector2.MoveTowards(transform.position, waypoint, speed);
 			rigidbody2D.MovePosition(p);
 		}
-		
-		else {
-			AI = true;
-		}
+
+		// if at waypoint, run AI module
+		else  AI = true;
+
 	}
 
 	void RunAway()
 	{
 
+	}
+
+	//------------------------------------------------------------------------------
+	// Utility functions
+	void MoveToWaypoint(bool loop = false)
+	{
+		waypoint = waypoints.Peek();		// get the waypoint
+		if(transform.position != waypoint)	// if its not reached
+		{									// move towards it
+			Vector2 p = Vector2.MoveTowards(transform.position, waypoint, speed);
+			rigidbody2D.MovePosition(p);
+		}
+		else 	// if waypoint is reached, remove it from the queue
+		{
+			if(loop)	waypoints.Enqueue(waypoints.Dequeue());
+			else		waypoints.Dequeue();
+		}
 	}
 }
