4d9efb5f119529102be695c06cae0e1c64255423
vilbeyli <volkanilbeyli@gmail.com>
2015-04-21 05:55:32 +0300

    Unity5 upgrade

---
+++ Assets/Scripts/Managers/NewBehaviourScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    //======================================
+    // Variable Declarations
+    
+    // static variables
+	
+	// handles
+	
+    // private variables
+
+    // public variables
+
+
+    //======================================
+    // Function Definitions
+
+    // getters & setters
+
+    // unity functions
+	void Awake ()
+	{
+	
+	}
+	
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+	
+	}
+
+    // member functions
+	
+}
